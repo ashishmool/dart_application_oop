0d16e7b0875d5f80314f503f4ae28d14bbfc20b9
Ashish Mool <asis.mool@gmail.com>
2024-11-22 16:13:44 +0545

    first commit

---
+++ bin/dart_application_oop.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_oop/dart_application_oop.dart' as dart_application_oop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_oop.calculate()}!');
+}
