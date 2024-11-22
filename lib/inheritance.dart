c53bb5e49d7655e3cd7ad9bd04f1fabdbcdd9039
Ashish Mool <asis.mool@gmail.com>
2024-11-22 16:24:46 +0545

    Inheritance
    -- Simple program to demonstrate understanding of Inheritance

---
+++ lib/inheritance.dart
@@ -0,0 +1,21 @@
+// inheritance.dart
+
+class Shape {
+  void draw() {
+    print("Drawing a shape");
+  }
+}
+
+class Circle extends Shape {
+  @override
+  void draw() {
+    print("Drawing a circle");
+  }
+}
+
+class Rectangle extends Shape {
+  @override
+  void draw() {
+    print("Drawing a rectangle");
+  }
+}
